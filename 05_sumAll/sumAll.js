a6deca731faf7a939890b5bcdea703c303b530e1
jin9669 <wooyujin@gmail.com>
2024-03-23 10:49:39 +0800

    added solutions up to test4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start < end && start >= 0){
+for (let i = start; i<=end; i++){
+sum+=i;}}
+    else if (start > end){
+for (let i = start; i>=end; i--){
+sum+=i;
+}
+    }
+    else {
+    return "ERROR";
+    }
+return sum;
 };
 
 // Do not edit below this line

3a8c155e6f079b70ec74d77d6e9308922ed38dbe
jin9669 <wooyujin@gmail.com>
2024-03-24 05:13:31 +0800

    Added solutions for ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,21 +1,18 @@
-// step 1: convert to array 
-// step 2: reverse array
-// step 3: convert reversed array back to string
-// step 4: if string = reversed string, return true otherwise return false
 
 
 
 const palindromes = function (string) {
-    let array = Array.from(string);
-    let reverseArray = array.reverse();
-    let reverseString = reverseArray.join('');
-    if (reverseString === string){
-        return true;
-    }
-    else{
-        return false;
-    }
+    let alphanumeric = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let array = Array.from(string.toLowerCase());
+    let filteredArray = array.filter((char) => alphanumeric.includes(char));
+    let filteredString = filteredArray.join('');
+    console.log(filteredString);
 
+    let reversedFilteredArray = filteredArray.reverse();
+    let reversedFilteredString = reversedFilteredArray.join('');
+    console.log(reversedFilteredString)
+
+    return filteredString === reversedFilteredString
 };
 
 // Do not edit below this line

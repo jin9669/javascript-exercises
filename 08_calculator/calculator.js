7f1447219eb257b3c65bf0931704d0e2d102a2b1
jin9669 <wooyujin@gmail.com>
2024-03-24 03:54:07 +0800

    Added solutions for ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(first, second) {
+return first+second;
+};
+
+const subtract = function(first, second) {
+  return first-second;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+      for (i=0; i<array.length; i++){
+    sum+=array[i];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(array2) {
+  let multiple = 1;
+    for (i=0; i<array2.length; i++){
+      multiple *= array2[i];
+    }
+  return multiple;
 };
 
-const multiply = function() {
-
+const power = function(first, second) {
+  return Math.pow(first,second);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(value) {
+  let final = 1; 
+  for (let i=1; i<=value; i++){
+    final *= i; 
+  }
+  return final;
 };
 
 // Do not edit below this line
